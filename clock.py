8fa8e9ab3c0d41355e3d62f4037296f81a507647
07sumit1002 <98510898+07sumit1002@users.noreply.github.com>
2023-02-18 20:10:27 +0530

    Add files via upload

---
+++ clock.py
@@ -0,0 +1,27 @@
+import sys
+from tkinter import *
+#import time library to obtain current time
+import time
+#create a function timing and variable current_time
+def timing():
+    #display current hour,minute,seconds
+    current_time = time.strftime("%H : %M : %S")
+    #configure the clock
+    clock.config(text=current_time)
+    #clock will change after every 200 microseconds
+    clock.after(200,timing)
+#Create a variable that will store our tkinter window
+root=Tk()
+#define size of the window
+root.geometry("600x400")
+#create a variable clock and store label
+#First label will show time, second label will show hour:minute:second, third label will show the top digital clock
+clock=Label(root,font=("times",60,"bold"),bg="lightblue")
+clock.grid(row=2,column=2,pady=50,padx=100)
+timing()
+#create a variable for digital clock
+digital=Label(root,text="Sumit's Digital Clock",font="times 24 bold",bg="blue")
+digital.grid(row=0,column=2) 
+nota=Label(root,text="Hours        Minutes        Seconds",font="roman 15 bold", bg="blue")
+nota.grid(row=3,column=2)
+root.mainloop()
